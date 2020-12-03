19150bb31addf4074b4bb6b872ace52f6da9afc3
Shlomi.O <ShlomiO@CodeValue.net>
2020-12-03 19:47:44 +0200

    + ReactiveTests skeleton

---
+++ src/Rx.Net.Sandbox/ReactiveTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Rx.Net.Sandbox
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void UnitOfWork_StateUnderTest_ExpectedBehavior()
+        {
+            Assert.Pass();
+        }
+    }
+}
